c58c7a20ceae066829625c2e898d5015155ddc8d
Roseedee Cehleah <roseedee2002@gmail.com>
2023-06-12 01:44:06 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -3,4 +3,9 @@ money_format = [1000, 500, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.25]
 for t in money_format:
     r = money // t
     money = money % t
-    print(t, "bath : ", r)
+    if t > 10 and r > 0:
+        print("แบงค์ ", t, " จำนวน : ", r)
+    elif t > 0 and r > 0:
+        print("เหรียญ ", t, " จำนวน : ", r)
+if money > 0:
+    print(money, " ไม่มีเหรียญ")
