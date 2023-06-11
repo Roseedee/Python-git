2e5daacc825facf157424e4e00b333ba81fd9e32
Roseedee Cehleah <roseedee2002@gmail.com>
2023-06-12 01:32:42 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,22 +1,6 @@
-import socket
-
-HOST = 'localhost'  # The remote host
-PORT = 12345  # The same port as used by the server
-
-# create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# connect to the server
-s.connect((HOST, PORT))
-print('Connected to', HOST, 'on port', PORT)
-
-# send and receive messages
-while True:
-    message = input('Enter message: ')
-    s.sendall(message.encode())
-    data = s.recv(1024)
-    if not data: break
-    print('Received message:', data.decode())
-
-# close the connection
-s.close()
+money = float(input("Enter Money(Bath) : "))
+money_format = [1000, 500, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.25]
+for t in money_format:
+    r = money // t
+    money = money % t
+    print(t, "bath : ", r)
