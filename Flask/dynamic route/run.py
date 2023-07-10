caf73287fba2b49911eae4d6d27cfc1759694e7e
Roseedee Cehleah <roseedee2002@gmail.com>
2023-07-10 15:57:15 +0700

    update

--- Flask/dynamic route/run.py
+++ Flask/dynamic route/run.py
@@ -15,10 +15,5 @@ def about():
 def profile(yourName):
     return "<center><h1>Profile Page</h1><h2>Welcome : {}</h2></center>".format(yourName)
 
-#sample url : localhost:5000/profile/"enter your name"/"enter your lastname"
-@app.route("/profile/<yourName>/<yourLastname>")
-def profile1(yourName, yourLastname):
-    return "<center><h1>Profile Page</h1><h2>Welcome : {} {}</h2></center>".format(yourName, yourLastname)
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
